91aaf0c917912c5af48e8183dc846350170df45e
PeterAronZentai <peter.zentai@avangrade.com>
2015-05-24 15:31:32 +0100

    first commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('opportunistApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
